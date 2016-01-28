d5d2512f78f35948b6c0915c9724e4f448657833
acalder <aaron@aaroncalder.com>
2016-01-27 23:51:49 -0700

    Add comments to ImageMagic commands in convert.sh.

--- img/convert.sh
+++ img/convert.sh
@@ -2,7 +2,10 @@
 
 #TODO: Finish this script later. Not a priority now.
 # Requires installation of ImageMagick suite.
-# TODO: Can the following commands can be combined?
+# TODO: Can the following two commands can be combined?
+# Scale image to height 600 pixels
 # convert -resize x600 Sunflowers-large.jpg Sunflowers.jpg
+# Add sides with skyblue background to image without resizing image.
+# Resulting image is 1000x600
 # convert -gravity center -background skyblue -extent 1000 Sunflowers.jpg Sunflowers-wide.jpg
 
