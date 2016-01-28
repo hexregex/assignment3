38da2d54612e7efd6f0e798e49de63e02d487677
acalder <aaron@aaroncalder.com>
2016-01-27 23:46:15 -0700

    Recording the ImageMagic commands I used in a file to later use and automate carousel image formatting.

---
+++ img/convert.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#TODO: Finish this script later. Not a priority now.
+# Requires installation of ImageMagick suite.
+# TODO: Can the following commands can be combined?
+# convert -resize x600 Sunflowers-large.jpg Sunflowers.jpg
+# convert -gravity center -background skyblue -extent 1000 Sunflowers.jpg Sunflowers-wide.jpg
+
